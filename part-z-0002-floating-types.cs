997978f2e97276088bda31fb3872079d4089a569
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-21 19:57:15 +0300

    Create part-z-0002-floating-types.cs

---
+++ part-z-0002-floating-types.cs
@@ -0,0 +1,15 @@
+float rate1 = 22.0f / 7.0f;
+
+double rate2 = 22.0 / 7.0;
+
+decimal rate3 = 22.0m / 7.0m; // m -> money
+
+Console.WriteLine(rate1);
+Console.WriteLine(rate2);
+Console.WriteLine(rate3);
+
+// 3.142857
+// 3.142857142857143
+// 3.1428571428571428571428571429
+
+// dotnet run
