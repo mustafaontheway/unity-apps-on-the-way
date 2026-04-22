2f9c43f7a7cd435f3ab10346e94d965ddfe09d1c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-22 10:48:12 +0300

    Create part-z-0011-datetime2.cs

---
+++ part-z-0011-datetime2.cs
@@ -0,0 +1,16 @@
+var timeData = DateTime.Now;
+
+System.Console.WriteLine(timeData.Hour);
+
+System.Console.WriteLine(timeData.Minute);
+
+System.Console.WriteLine(timeData.Second);
+
+System.Console.WriteLine(timeData);
+
+// 10
+// 47
+// 37
+// 22/04/2026 10:47:37
+
+// dotnet run
