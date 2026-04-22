fcb6db7d20ff1185f76058a75ed592ba4801594a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-22 10:54:53 +0300

    Create part-z-0011-datetime4.cs

---
+++ part-z-0011-datetime4.cs
@@ -0,0 +1,15 @@
+DateTime endTime1 = new DateTime(2027, 3, 8);
+
+System.Console.WriteLine(endTime1);
+
+System.Console.WriteLine(endTime1.Month);
+
+DateTime endTime2 = new DateTime(2027, 3, 8, 21, 18, 15);
+
+System.Console.WriteLine(endTime2);
+
+// 08/03/2027 00:00:00
+// 3
+// 08/03/2027 21:18:15
+
+// dotnet run
