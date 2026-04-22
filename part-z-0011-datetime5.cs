4088f146d0f4f8931cf6bd82c15c50f9ef2369ac
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-22 11:02:43 +0300

    Create part-z-0011-datetime5.cs

---
+++ part-z-0011-datetime5.cs
@@ -0,0 +1,10 @@
+var delta = DateTime.Now - new DateTime(2025, 4, 18);
+
+System.Console.WriteLine(delta.TotalDays);
+
+System.Console.WriteLine(delta.TotalHours);
+
+// 369.45827524080903
+// 8866.998605779416
+
+// dotnet run
