0299a46f31b5aa819098918609c539e7a428bb22
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-22 10:45:09 +0300

    Create part-z-0011-datetime1.cs

---
+++ part-z-0011-datetime1.cs
@@ -0,0 +1,13 @@
+var timeData = DateTime.Now;
+
+System.Console.WriteLine(timeData.Day);
+
+System.Console.WriteLine(timeData.Month);
+
+System.Console.WriteLine(timeData.Year);
+
+// 22
+// 4
+// 2026
+
+// dotnet run
