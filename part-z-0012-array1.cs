2796cacdfd72c3652b78aaf1f99d8584f8eef0ff
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-22 20:26:24 +0300

    Create part-z-0012-array1.cs

---
+++ part-z-0012-array1.cs
@@ -0,0 +1,18 @@
+byte[] ages = {17, 74, 81, 99};
+
+string[] names = new string[5];
+
+names[0] = "Ayhan";
+names[1] = "Aykan";
+names[2] = "Ayman";
+names[3] = "Aykul";
+names[4] = "Ayben";
+
+System.Console.WriteLine(ages[1]);
+
+System.Console.WriteLine(names[2]);
+
+// 74
+// Ayman
+
+// dotnet run
