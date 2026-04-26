67fa5a5e1867cd2659210dcdd22a994d26d20dbb
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-26 21:05:55 +0300

    Create part-z-0014-array-slice2.cs

---
+++ part-z-0014-array-slice2.cs
@@ -0,0 +1,7 @@
+string[] names = {"Ayhan", "Aykan", "Ayman", "Aykul", "Ayben", "Aygül", "Aysel", "Aysu"};
+
+var namesSlice1 = names[2..6];
+
+var namesSlice2 = names[2..];
+
+var namesSlice3 = names[..4];
