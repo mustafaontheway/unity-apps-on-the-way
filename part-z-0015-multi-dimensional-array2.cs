bf3f99207a7512a1978c1d2764948aeeb8ebae4d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-26 21:29:46 +0300

    Create part-z-0015-multi-dimensional-array2.cs

---
+++ part-z-0015-multi-dimensional-array2.cs
@@ -0,0 +1,8 @@
+uint[,] studentGrades = {{55, 87, 98}, {77, 99, 66}};
+
+System.Console.WriteLine("Student 2 final grade: " + studentGrades[1, 2]);
+
+// Student 2 final grade: 66
+
+
+// dotnet run
