bbb9bd26ff60ffe35304d2e800243aa709c921b2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-27 09:55:57 +0300

    Create part-z-0016-ternary.cs

---
+++ part-z-0016-ternary.cs
@@ -0,0 +1,12 @@
+uint sales = 4_250_000;
+
+uint cost = 32_410_456;
+
+string salesResult = sales > cost ? "profit" : "loss";
+
+System.Console.WriteLine(salesResult); // loss
+
+
+
+
+// dotnet run 
