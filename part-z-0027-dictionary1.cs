8dbc5bfa0833fefa4122bc8958cc8d9cfe249d2d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-28 19:55:59 +0300

    Create part-z-0027-dictionary1.cs

---
+++ part-z-0027-dictionary1.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+Dictionary<string, byte> agesByNames = new Dictionary<string, byte> ();
+
+agesByNames.Add("Ayhan Bilir", 99);
+
+agesByNames.Add("Hakan Bilir", 19);
