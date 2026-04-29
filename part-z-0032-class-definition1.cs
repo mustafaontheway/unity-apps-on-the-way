628861defaf6a8d4d3fdc1b04d21426b149e7ec0
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-04-29 21:02:58 +0300

    Create part-z-0032-class-definition1.cs

---
+++ part-z-0032-class-definition1.cs
@@ -0,0 +1,19 @@
+Employee empAyhan = new Employee();
+
+empAyhan.name = "Ayhan Bilir";
+empAyhan.empID = "ab004254";
+empAyhan.department = "HR";
+empAyhan.salaryUSD = 4200;
+empAyhan.isMarried = false;
+
+
+public class Employee
+{
+    public string name;
+    public string empID;
+    public string department;
+    public ushort salaryUSD;
+    public bool isMarried;
+}
+
+// dotnet run
