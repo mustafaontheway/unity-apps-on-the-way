ee6d95824a240957439911f8058a9c9cf0cab8b2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-06 18:30:09 +0300

    Create part-z-0041-binary-representation1.cs

---
+++ part-z-0041-binary-representation1.cs
@@ -0,0 +1,13 @@
+byte age = 17;
+
+ushort year = 2026;
+
+System.Console.WriteLine(Convert.ToString(age, 2));
+
+System.Console.WriteLine(Convert.ToString(year, 2));
+
+// 10001 -> 16 + 1
+// 11111101010 -> 1024 + 512 + 256 + 128 + 64 + 32 + 8 + 2
+
+
+// dotnet run
