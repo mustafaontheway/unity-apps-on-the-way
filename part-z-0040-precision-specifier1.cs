f4ed12251b98c3c34b9317fc69de6209c593cb1f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-06 11:21:21 +0300

    Create part-z-0040-precision-specifier1.cs

---
+++ part-z-0040-precision-specifier1.cs
@@ -0,0 +1,11 @@
+var rate = 17.212154777777777777777777777m;
+
+System.Console.WriteLine($"Interest rate: {rate:F2}");
+System.Console.WriteLine($"Interest rate: {rate:F4}");
+System.Console.WriteLine($"Interest rate: {rate:F8}");
+
+// Interest rate: 17.21
+// Interest rate: 17.2122
+// Interest rate: 17.21215478
+
+//dotnet run
