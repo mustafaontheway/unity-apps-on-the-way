3f66b89e92ee9f2dcb59039b7541361261b55582
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-05-06 08:45:56 +0300

    Create part-z-0007-casting2.cs

---
+++ part-z-0007-casting2.cs
@@ -0,0 +1,14 @@
+var x = 17;
+
+var y = 14;
+
+var r1 = x / y;
+
+System.Console.WriteLine(r1); // 1
+
+double r2 = (double) x / y;
+
+System.Console.WriteLine(r2); // 1.2142857142857142
+
+
+//dotnet run
